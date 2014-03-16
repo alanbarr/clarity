f4a82ffe181611b75821bfe37f719d8f5f47ea0e
Alan Barr <alanbarr2002@hotmail.com>
2014-03-16 22:33:00 +0000

    build sntp

--- clarity.mk
+++ clarity.mk
@@ -3,8 +3,8 @@ CLARITY_SRC = $(CLARITY_DIR)/src/http_proto.c       \
 			  $(CLARITY_DIR)/src/http_handler.c     \
 			  $(CLARITY_DIR)/src/clarity.c			\
 			  $(CLARITY_DIR)/src/mgmt.c				\
-			  $(CLARITY_DIR)/src/http_server.c
-
+			  $(CLARITY_DIR)/src/http_server.c		\
+			  $(CLARITY_DIR)/src/sntp.c
 
 CLARITY_INC = $(CLARITY_DIR)/src/ \
 			  $(CLARITY_DIR)/api/ 
