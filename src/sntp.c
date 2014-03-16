2b0d5612758bcce7ba93f683cebebe4857b6f494
Alan Barr <alanbarr2002@hotmail.com>
2014-03-16 22:29:00 +0000

    SNTP should be close to functional - needs testing.

--- src/sntp.c
+++ src/sntp.c
@@ -25,7 +25,12 @@
 *******************************************************************************/
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
+#include "clarity_api.h"
+#include "clarity_int.h"
+#include "socket.h"
 
+#if 0
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -37,6 +42,7 @@ typedef int32_t clarityError;
 #define CLARITY_SUCCESS 0
 #define CLARITY_ERROR_BUFFER_SIZE !CLARITY_SUCCESS
 #define CLARITY_ERROR_REMOTE_RESPONSE CLARITY_ERROR_BUFFER_SIZE
+#endif
 
 #define SNTP_PORT                       123
 #define SNTP_SERVER                     "pool.ntp.org"
@@ -55,6 +61,8 @@ typedef int32_t clarityError;
 
 #define NTP_SECONDS_INDEX               0
 #define NTP_SECONDS_FRACTION_INDEX      1
+
+#define NTP_UNIX_EPOCH_DIFFERENCE       2208988800
 
 #define compile_time_assert(X)                                              \
     extern int (*compile_assert(void))[sizeof(struct {                      \
@@ -115,63 +123,74 @@ static clarityError sntpConstructRequest(char * buf)
 
 clarityError sntpRequest(char * buf)
 {
-    int sockfd;
-    struct sockaddr_in their_addr;
-    int addr;
-    struct hostent *he;
-    int numbytes;
-    int addrlen = sizeof(struct sockaddr);
+    int32_t sockfd = -1;
+    sockaddr_in serverAddr;
+    int16_t bytes = -1;
+    socklen_t addrlen = sizeof(serverAddr);
+    clarityError rtn = CLARITY_ERROR_UNDEFINED;
 
-    if ((he=gethostbyname(SNTP_SERVER)) == NULL) 
-    {
-        printf("%s %d exit!", __FILE__, __LINE__);
-        exit(1);
-    }
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) 
-    {
-        perror("socket");
-        exit(1);
-    }
-    their_addr.sin_family = AF_INET;
-    their_addr.sin_port = htons(SNTP_PORT);
-    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-    memset(&(their_addr.sin_zero), 0, 8);
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SNTP_PORT);
 
-    if ((numbytes=sendto(sockfd, buf, NTP_PACKET_HEADER_SIZE, 0,
-                         (struct sockaddr *)&their_addr,
-                         sizeof(struct sockaddr))) == -1) 
+    if ((rtn = clarityMgmtRegisterProcessStarted()) != CLARITY_SUCCESS)
     {
-        perror("sendto");
-        exit(1);
+        return rtn;
     }
     
-    printf("sent %d bytes\n", numbytes);
-
-    if ((numbytes = recvfrom(sockfd, buf, NTP_PACKET_HEADER_SIZE, 0,
-                            (struct sockaddr *)&their_addr,
-                            &addrlen)) == -1)
+    else if (gethostbyname(SNTP_SERVER, strlen(SNTP_SERVER),
+                           &serverAddr.sin_addr.s_addr) < 0) 
     {
-        perror("recvfrom");
-        exit(1);
+        rtn = CLARITY_ERROR_CC3000_SOCKET;
     }
 
-    printf("rx %d bytess\n", numbytes);
-    close(sockfd);
-    return CLARITY_SUCCESS;
+    else if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) 
+    {
+        rtn = CLARITY_ERROR_CC3000_SOCKET;
+    }
+
+    else if ((serverAddr.sin_addr.s_addr = htonl(serverAddr.sin_addr.s_addr))== 0)
+    {
+        rtn = CLARITY_ERROR_UNDEFINED;
+    }
+
+    else if ((bytes = sendto(sockfd, buf, NTP_PACKET_HEADER_SIZE, 0,
+                             (sockaddr *)&serverAddr, sizeof(sockaddr)))
+                    != NTP_PACKET_HEADER_SIZE) 
+    {
+        rtn = CLARITY_ERROR_CC3000_SOCKET;
+    }
+    
+    else if ((bytes = recvfrom(sockfd, buf, NTP_PACKET_HEADER_SIZE, 0,
+                               (sockaddr *)&serverAddr, &addrlen))
+                    != NTP_PACKET_HEADER_SIZE)
+    {
+        rtn = CLARITY_ERROR_CC3000_SOCKET;
+    }
+
+    if (sockfd != -1)
+    {
+        if (closesocket(sockfd) != 0)
+        {
+            rtn = CLARITY_ERROR_CC3000_SOCKET;
+        }
+    }
+
+    rtn = clarityMgmtRegisterProcessFinished();
+    return rtn;
 }
 
 
 clarityError clarityGetSntpTime(char * buf, uint16_t bufSize, uint32_t * ntpTime)
 {
-    clarityError rtn;
+    clarityError rtn = CLARITY_ERROR_UNDEFINED;
 
     if (bufSize < NTP_PACKET_HEADER_SIZE)
     {
         return CLARITY_ERROR_BUFFER_SIZE;
     }
 
-    if ((rtn = sntpConstructRequest(buf))
-             != CLARITY_SUCCESS)
+    if ((rtn = sntpConstructRequest(buf)) != CLARITY_SUCCESS)
     {
         return rtn;
     }
@@ -189,11 +208,55 @@ clarityError clarityGetSntpTime(char * buf, uint16_t bufSize, uint32_t * ntpTime
     return CLARITY_SUCCESS;
 }
 
+#if 0
 int main(void)
 {
     char buf[NTP_PACKET_HEADER_SIZE];
     uint32_t t = 0;
+    time_t st = time(NULL);
+    struct tm timeStruct;
+    struct tm timeStruct1;
+    time_t temp;
+
     printf("rtn: %d\n", clarityGetSntpTime(buf, sizeof(buf),&t));
     printf("time: %u %x\n", t, t);
+    t = t - NTP_UNIX_EPOCH_DIFFERENCE;
+
+    printf("time: %u %x\n", t, t);
+    printf("system time: %u %x\n", st, st);
+
+    temp = t;
+    if (gmtime_r(&temp, &timeStruct) == NULL)
+    {
+        printf("shit.\n");
+    }
+    
+    if (gmtime_r(&st, &timeStruct1) == NULL)
+    {
+        printf("shit.\n");
+    }
+
+    printf("tm_sec  :%d\n",     timeStruct.tm_sec);
+    printf("tm_min  :%d\n",     timeStruct.tm_min);
+    printf("tm_hour :%d\n",     timeStruct.tm_hour);
+    printf("tm_mday :%d\n",     timeStruct.tm_mday);
+    printf("tm_mon  :%d\n",     timeStruct.tm_mon);
+    printf("tm_year :%d\n",     timeStruct.tm_year);
+    printf("tm_wday :%d\n",     timeStruct.tm_wday);
+    printf("tm_yday :%d\n",     timeStruct.tm_yday);
+    printf("tm_isdst:%d\n",     timeStruct.tm_isdst);
+    printf("\n");
+    printf("tm_sec  :%d\n",     timeStruct1.tm_sec);
+    printf("tm_min  :%d\n",     timeStruct1.tm_min);
+    printf("tm_hour :%d\n",     timeStruct1.tm_hour);
+    printf("tm_mday :%d\n",     timeStruct1.tm_mday);
+    printf("tm_mon  :%d\n",     timeStruct1.tm_mon);
+    printf("tm_year :%d\n",     timeStruct1.tm_year);
+    printf("tm_wday :%d\n",     timeStruct1.tm_wday);
+    printf("tm_yday :%d\n",     timeStruct1.tm_yday);
+    printf("tm_isdst:%d\n",     timeStruct1.tm_isdst);
+
     return 0;
 }
+#endif
+
